5944e06b99729f9f1fdd46a58574fe022f3419a1
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2023-10-10 21:35:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const PORT = 5000
 
 const initServer(port){
   const app = express()
+  
   app.get("/", (req, res)=>{
     res.json({status: "ok", message: "hey"}
   })
+
+  app.listen(PORT, () => { console.log(`server started on port ${port}`) })
 }
 
 initServer(PORT)
